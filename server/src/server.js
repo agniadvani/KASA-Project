315e0fbff2fd921f5e25d94724f44402602d5382
agniadvani <advani.agni.agni@gmail.com>
2022-07-01 19:49:42 +0530

    npm scripts

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,21 @@
 const http = require('http')
 const app = require('./app')
+const { loadPlanetData } = require("./models/planets.model")
 const PORT = process.env.PORT || 8000
 
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`);
-})
+async function listen() {
+    try {
+        await loadPlanetData()
+    } catch (e) {
+        console.error("Could not load planet data " + e)
+    }
+
+    server.listen(PORT, () => {
+        console.log(`Listening on PORT: ${PORT}`);
+    })
+}
+
+listen()
+
