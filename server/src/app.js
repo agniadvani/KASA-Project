fa6c3399fb06a6a41f4a5df00309fcba65f3525d
agniadvani <advani.agni.agni@gmail.com>
2022-07-01 21:01:35 +0530

    npm build and deploy

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const planetRouter = require('./routes/planets/planets.router')
 const app = express()
 
@@ -8,6 +9,11 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "..", "public")))
 app.use(planetRouter)
+
+app.get("/", (req,res)=>{
+    res.sendFile("../public/index.html")
+})
 
 module.exports = app
